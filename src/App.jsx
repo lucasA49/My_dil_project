4e03e16fd1c0ad0e2756e1a235653ed71a5b0448
Lucasa49 <lucasaksu@gmail.com>
2024-09-24 17:41:06 +0200

    mise en place du formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
-
+import Connexion from './pages/Connexion';
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Sidebar/>
-            <Footer/>
-        </Router>
-    );
+  return (
+    <Router>
+      {/* Structure de la page commune, comme le Sidebar et Footer */}
+      <Sidebar />
+
+      {/* Routes définies ici */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="connexion" element={<Connexion />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Connexion.jsx
@@ -0,0 +1,15 @@
+import Sign from '../components/Signin';
+import Sidebar from '../components/Sidebar';
+
+function Connexion() {
+    return (
+        <>
+        <Sign />
+        <Sidebar />
+    
+        </>
+        
+    )
+}
+
+export default Connexion
