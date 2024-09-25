af6041f711550fb54e29f993cc55aa82a371f0e3
Mathieu.INGREMEAU <mathieu.ingremeau@gmail.com>
2024-09-25 11:07:44 +0200

    form fini (excepté background)

--- src/components/Project_Form.jsx
+++ src/components/Project_Form.jsx
@@ -3,17 +3,26 @@ import '../styles/Project_Form.css';
 const P_Form = () => {
     return (
         <div className="main-content">
-            <h1><u>Créer</u> mon projet</h1>
-            <form>
-                <h3 className='form-titles'>Nom du projet</h3>
-                <input type='text' name="project_name" />
-                <h3 className='form-titles'>Compétences</h3>
-                <input type='text' name="project_skills" />
-                <h3 className='form-titles'>Description</h3>
-                <textarea name="project_description" id=""></textarea>
-
-                <input type="submit" name="project_form_submit" />
-            </form>
+            <div className="formpage">
+                <h1><u>Créer</u> mon projet</h1>
+                <form>
+                    <div className='formbox'>
+                        <h3 className='form-titles'>Nom du projet</h3>
+                        <input type='text' name="project_name" className='inputform' />
+                    </div>
+                    <div className='formbox'>
+                        <h3 className='form-titles'>Compétences</h3>
+                        <input type='text' name="project_skills" className='inputform' />
+                    </div>
+                    <div className='formbox'>
+                        <h3 className='form-titles'>Description</h3>
+                        <textarea name="project_description" className='project_description'></textarea>
+                    </div>
+                    <div className='submitbox'>
+                        <input type="submit" name="project_form_submit" className='project_form_submit' value="Créer" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
