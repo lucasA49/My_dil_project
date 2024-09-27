3d1a5ab9fccd8725bfe34da64f31abf37de7c9f4
Mathieu.INGREMEAU <mathieu.ingremeau@gmail.com>
2024-09-27 11:01:28 +0200

    Changements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
-import Connexion from './pages/Connexion';
+import { Home } from './pages/Home';
+import { Sidebar } from './components/Sidebar';
+import { Footer } from './components/Footer';
+import { Connexion } from './pages/Connexion';
+import { Project_Form } from './pages/Project_Form';
+
 function App() {
     return (
         <Router>
@@ -20,3 +22,5 @@ function App() {
         </Router>
     );
 }
+
+export default App;
