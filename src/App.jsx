e84818fcd543fd7b0b5fcb5dd9b9e166f1aeea2a
Mathieu.INGREMEAU <mathieu.ingremeau@gmail.com>
2024-09-27 11:02:48 +0200

    App fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Sidebar } from './components/Sidebar';
-import { Footer } from './components/Footer';
-import { Connexion } from './pages/Connexion';
-import { Project_Form } from './pages/Project_Form';
+import Home from './pages/Home';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Connexion from './pages/Connexion';
+import Project_Form from './pages/Project_Form';
 
 function App() {
     return (
